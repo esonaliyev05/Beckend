e61e4b8099cd8ccb55c2041e0c7ef034d1660309
esonaliyev05 <esonaliyevalyorbek911@gmail.com>
2026-01-12 18:02:48 +0500

    new change HTTP methods

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
+
+
+
+
 const express = require('express')
+
 const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello Alyor!" })
+
+})
+
+app.post("/post", (req, res) => {
+    const {firstName , lastName} = req.body
+    const message = `His full name - ${req.body.firstName} ${req.body.lastName}`
+    res.send(message)
+})
+
+app.delete(`/:id` , (req , res) => {
+    const {id} = req.params
+    res.send(id)
+
+})
+
+app.put("/:id" , (req , res) => {
+    const {id} = req.params
+    const body = req.body
+
+    res.json({id, body})
+})
+
 const PORT = 8080
 app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`))
+
